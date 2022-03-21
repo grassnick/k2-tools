cb95454cb9c1e84d5eaef35f1d0102b44604fa4b
Georg Graßnick <georg.grassnick@tu-dresden.de>
2022-03-21 20:06:54 +0000

    Add background processes

--- app.cpp
+++ app.cpp
@@ -4,13 +4,22 @@ extern "C" {
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <sys/resource.h>
+#include <sys/wait.h>
 }
 
 #include <cerrno>
 #include <cstring>
 #include <iostream>
 #include <string>
+#include <thread>
+#include <chrono>
 #include <functional>
+#include <csignal>
+#include <memory>
+
+
+using namespace std::chrono_literals;
 
 #include "k2.h"
 
@@ -90,7 +99,8 @@ namespace k2 {
         return version;
     }
 
-    std::string getActiveDevices() {
+    std::string getActiveDevices()
+    {
         int ret = 0;
         const std::string device;
         std::string instances;
@@ -144,8 +154,25 @@ namespace k2 {
                     std::cerr << "ioctl unregister periodic task failed: " << strerror(errno)
                               << std::endl;
                 } else {
-                    std::cout << "Unegistered periodic task with pid " << io.task_pid << " for " << io.blk_dev
+                    std::cout << "Unregistered periodic task with pid " << io.task_pid << " for " << io.blk_dev
                               << std::endl;
+                }
+            });
+        });
+    }
+
+    void unregisterAllTasks(const std::string &device)
+    {
+        int ret = 0;
+        dynamicDeviceContext([&](int &fd) {
+            dynamicIoctlContext(device, [&](struct k2_ioctl &io) {
+
+                ret = ioctl(fd, K2_IOC_UNREGISTER_ALL_PERIODIC_TASKS, &io);
+                if (ret < 0) {
+                    std::cerr << "ioctl unregister all periodic tasks failed: " << strerror(errno)
+                              << std::endl;
+                } else {
+                    std::cout << "Unregistered all periodic tasks for " << io.blk_dev << std::endl;
                 }
             });
         });
@@ -153,18 +180,120 @@ namespace k2 {
 
 }
 
+// Global variables <3
+const std::string disk("nvme0n1");
+int fd = 0;
+void *buffer = nullptr;
+constexpr std::size_t NUM_BACKGROUND_PROCESSES = 8;
+std::array<pid_t, NUM_BACKGROUND_PROCESSES> backgroundProcessPids;
+bool childTerminate = false;
+
+
+void terminate()
+{
+    std::cerr << "Process terminating gracefully" << std::endl;
+    k2::unregisterAllTasks(disk);
+    k2::closeDriver(fd);
+    if (buffer != nullptr) {
+        free(buffer);
+    }
+    exit(-1);
+}
+
+void mainSignalHandler(int signal)
+{
+    terminate();
+}
+
+void childSignalHandler(int signal)
+{
+    childTerminate = true;
+}
+
+void backgroundLoad(int index)
+{
+    std::signal(SIGINT, childSignalHandler);
+    std::signal(SIGTERM, childSignalHandler);
+
+    const std::size_t bs = 1024 << 5; // 1M
+    void *childBuffer = malloc(bs);
+    memset(childBuffer, 0, bs);
+
+    // Set different prio levels to children
+    setpriority(PRIO_PROCESS, 0, index);
+
+    int f = open(("/dev/" + disk).c_str(), O_RDWR | O_SYNC);
+    if (f < 0) {
+        std::cout << "Could not open raw device file" << std::endl;
+    } else {
+        while (!childTerminate) {
+            write(f, childBuffer, bs);
+            std::this_thread::sleep_for(100ns);
+        }
+    }
+    close(fd);
+    free(childBuffer);
+}
+
 
 int main()
 {
-    const auto disk = "nvme0n1";
-    const auto pid = 8000;
-    const auto interval = 1337;
+    for (std::size_t i = 0; i < NUM_BACKGROUND_PROCESSES; i++) {
+        const pid_t forkPid = fork();
+        if (forkPid < 0) {
+            std::cerr << "Fork failed" << std::endl;
+            terminate();
+        } else if (forkPid == 0) {
+            // Child process
+            backgroundLoad(i);
+            return 0;
+        } else {
+            // Parent process
+            std::cout << "Starting background load on pid " << forkPid << std::endl;
+            backgroundProcessPids[i] = forkPid;
+        }
+    }
+
+    const auto mainPid = getpid();
+    const auto interval = 100000000;
+
+    std::signal(SIGINT, mainSignalHandler);
+    std::signal(SIGTERM, mainSignalHandler);
+
+    // Assign the highest possible priority to this process
+    setpriority(PRIO_PROCESS, 0, PRIO_MAX);
 
     std::cout << "k2 version is " << k2::getVersion() << std::endl;
     std::cout << "k2 is active on " << k2::getActiveDevices() << std::endl;
-    k2::registerTask(disk, pid, interval);
-    k2::unregisterTask(disk, pid);
+    k2::registerTask(disk, mainPid, interval);
 
+    // Allocate memory for buffer to write
+    const std::size_t bs = 1024 << 8; // 256K
+    buffer = malloc(bs); // This mem
+    memset(buffer, 0, bs);
+
+    fd = open(("/dev/" + disk).c_str(), O_RDWR | O_SYNC);
+    if (fd < 0) {
+        std::cout << "Could not open raw device file" << std::endl;
+    } else {
+
+        for (std::size_t i = 0; i < 1024; i++) {
+            write(fd, buffer, bs);
+            std::this_thread::sleep_for(interval * 1ns);
+        }
+    }
+    std::this_thread::sleep_for(3s);
+
+    // Close all background load processes
+    for (const auto pid: backgroundProcessPids) {
+        kill(pid, SIGTERM);
+        int stat;
+        waitpid(pid, &stat, 0);
+    }
+
+    k2::unregisterTask(disk, mainPid);
+    k2::closeDriver(fd);
+    free(buffer);
 
     return 0;
 }
